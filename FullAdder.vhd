61b75ceea002b41a77405d4ee3553e8716c41563
NavidFarahmand <NavidFarahmand75@gmail.com>
2017-07-03 19:06:47 +0430

    Initial commit for fpga project

---
+++ FullAdder.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/03/2017 07:06:08 PM
+-- Design Name: 
+-- Module Name: FullAdder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FullAdder is
+--  Port ( );
+end FullAdder;
+
+architecture Behavioral of FullAdder is
+
+begin
+
+
+end Behavioral;
